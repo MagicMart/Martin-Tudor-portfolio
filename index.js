ac7ff3288cbe950373c606f8af2dc88ae1eac383
Martin Tudor <martcan27@gmail.com>
2018-12-02 21:27:21 +0000

    Update app

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-"use strict";
+import "./main.css";
 
-var featuredArr = [
+const featuredArr = [
     {
-        image: "imgs/memory-game-cards.png",
+        image: "./src/imgs/memory-game-cards.png",
         alt: "Memory game cards",
         link: "https://magicmart.github.io/memory-game-cards/",
         title: "Memory Game Cards"
     },
     {
-        image: "imgs/arcade-game.png",
+        image: "./src/imgs/arcade-game.png",
         alt: "Game action from the arcade game",
         link: "https://magicmart.github.io/martin-arcade-game/",
         title: "Arcade Game"
     },
     {
-        image: "imgs/robin.png",
+        image: "./src/imgs/robin.png",
         alt: "European Robin",
         link: "https://codepen.io/MagicMart/full/LOLjJB/",
         title: "Animal Trading Card"
     },
     {
-        image: "imgs/pixel-react.png",
+        image: "./src/imgs/pixel-react.png",
         alt: "Abstract art created with pixel-react",
         link: "https://github.com/MagicMart/pixel-react",
         title: "Pixel Art Maker"
     },
     {
-        image: "imgs/gliders.png",
+        image: "./src/imgs/gliders.png",
         alt: "A still image from The Game Of life app",
         link: "https://magicmart.github.io/Game-of-Life/",
         title: "Game of Life"
@@ -35,24 +35,17 @@ var featuredArr = [
 
 function featured(arr) {
     "use strict";
-    var stuff = document.getElementById("featured");
-    var frag = document.createDocumentFragment();
-    arr.forEach(function(elem) {
-        var div = document.createElement("div");
+    const stuff = document.getElementById("featured");
+    const frag = document.createDocumentFragment();
+    arr.forEach(elem => {
+        const div = document.createElement("div");
         div.classList = "col-4 featured container-padding";
-        div.setAttribute("role", "listitem");
-        div.setAttribute("aria-labelledby", "featured");
         div.insertAdjacentHTML(
             "beforeend",
-            '<img src="' +
-                elem.image +
-                '" class="row" alt="' +
-                elem.alt +
-                '">\n       <a href="' +
-                elem.link +
-                '" class="row">  \n         <h3>' +
-                elem.title +
-                "</h3>\n        </a>"
+            `<img src="${elem.image}" class="row" alt="${elem.alt}">
+         <a href="${elem.link}" class="row">  
+           <h3>${elem.title}</h3>
+          </a>`
         );
         frag.appendChild(div);
     });
